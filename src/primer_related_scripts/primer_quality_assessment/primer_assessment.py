068c287d3601499ffa18502160ea5c285e9097f8
Maryam Ahmadi <mary.elf97@gmail.com>
2024-05-15 10:33:47 -0700

    fixed the code to create isolate-wise amplicon length files

--- src/primer_related_scripts/primer_quality_assessment/primer_assessment.py
+++ src/primer_related_scripts/primer_quality_assessment/primer_assessment.py
@@ -1,26 +1,41 @@
 import os
-
-import pandas as pd
+import argparse
 from Bio import SeqIO
+import pandas as pd
 
-primer_versions = ["V2"]
-amplicon_names = pd.read_csv("/home/mahmadi/tb_seqs/primers/primer_names.txt", names= ["amplicon_name"])
-isolates = pd.read_csv("/home/mahmadi/tb_seqs/assemblies/long_read_assemblies/pacbio-RSII/isolates.txt", names= ["isolate_name"])
+def calculate_sequence_lengths(fasta_folder):
+    all_sequences = []
+    for file_name in os.listdir(fasta_folder):
+        if file_name.endswith(".fasta"):
+            file_path = os.path.join(fasta_folder, file_name)
+            sequences = calculate_sequence_length(file_path, file_name)
+            all_sequences.extend(sequences)
+    return all_sequences
 
-df = pd.DataFrame(columns=["primer_version","amplicon_name","amplicon_length"])
-for isolate in isolates["isolate_name"]:
-    for primer in primer_versions:
-        for i in range(0,164):
-            path = ("/home/mahmadi/tb_seqs/seq_simulation/amplicons/hybrid/primer_V1/GCA_014900175.1/amplicons/" +
-                    isolate + "/amplicons/" + isolate + "_amplicon_" + str(i) + ".fasta")
-            if os.path.exists(path):
-                with open(path, "r") as handle:
-                    for record in SeqIO.parse(handle, "fasta"):
-                        new_row = {"isolate" : [isolate], "primer_version": [primer],"amplicon_name": "amplicon_" +  str(i),"amplicon_length": [len(record.seq)]}
-                        df = pd.concat([df, pd.DataFrame(new_row)], ignore_index=True)
-            else:
-                new_row = {"isolate": [isolate], "primer_version": [primer], "amplicon_name": "amplicon_" + str(i),
-                           "amplicon_length": "NA"}
-                df = pd.concat([df, pd.DataFrame(new_row)], ignore_index=True)
+def calculate_sequence_length(fasta_file, file_name):
+    sequences = []
+    for record in SeqIO.parse(fasta_file, "fasta"):
+        sequence_id = file_name
+        sequence_length = len(record.seq)
+        sequences.append((sequence_id, sequence_length))
+    return sequences
 
-df.to_csv("/home/mahmadi/tb_seqs/primers/primer_stats_hybrid.csv", index= False)
+def create_dataframe(sequences):
+    df = pd.DataFrame(sequences, columns=['sequence_id', 'sequence_length'])
+    return df
+
+def main():
+    parser = argparse.ArgumentParser(description="Calculate sequence lengths from FASTA files in a folder and store them in a DataFrame.")
+    parser.add_argument("input_folder", help="Input folder containing FASTA files")
+    parser.add_argument("output_file", help="Output CSV file path")
+    args = parser.parse_args()
+
+    input_folder = args.input_folder
+    output_file = args.output_file
+
+    sequences = calculate_sequence_lengths(input_folder)
+    df = create_dataframe(sequences)
+    df.to_csv(output_file, index=False)
+
+if __name__ == "__main__":
+    main()
