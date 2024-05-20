90a434615971329da596ef714938a8176fbfffb5
Maryam <mary.elf97@gmail.com>
2024-05-20 10:25:51 -0700

    modified the code to create both amplicon length dataframes and amplicon sequece fasta files

--- src/create_amplicons/pattern_match/create_amplicons.py
+++ src/create_amplicons/pattern_match/create_amplicons.py
@@ -1,3 +1,4 @@
+import math
 import os
 from os.path import basename
 from Bio import SeqIO
@@ -9,21 +10,20 @@ import argparse
 
 def find_closest_match(pattern,reference_seq):
     """function to find a string allowing up to 3 mismatches"""
-    for m in regex.finditer(r"\L<primer_string>{s<=2}",
-                            reference_seq, primer_string=[pattern]):
+    for m in regex.finditer(r"\L<primer_string>{s<=2,i<1,d<1}",
+                                reference_seq, primer_string=[pattern]):
         return m.start()
 
 
 def make_amplicon(left_primer_loc,right_primer_loc,
-                  alt_left_primer_loc, alt_right_primer_loc,
                   primer_seq_y, reference):
     """function to create amplicons based on the string match location"""
-    if left_primer_loc == "-1" or right_primer_loc == "-1":
-        amplicon = reference[alt_left_primer_loc - 1: alt_right_primer_loc + len(primer_seq_y)]
+    if math.isnan(left_primer_loc) or math.isnan(right_primer_loc):
+        amplicon = ""
+        return amplicon
     else:
-        amplicon = reference[left_primer_loc - 1: right_primer_loc + len(primer_seq_y)]
-    return amplicon
-
+        amplicon = reference[int(left_primer_loc - 1): int(right_primer_loc + len(primer_seq_y))]
+        return amplicon
 
 def main():
     parser = argparse.ArgumentParser(description="Create amplicons for a genome using a primer set.")
@@ -53,26 +53,30 @@ def main():
                                  merged_df[merged_df['amplicon_length'] == 0],
                                   merged_df[merged_df['amplicon_length'] < 0]])
     failed_amplicons["alt_left_primer_loc"] = failed_amplicons.apply(lambda row: find_closest_match(row["primer_seq_x"],reference_seq), axis=1)
-    failed_amplicons["alt_right_primer_loc"] = failed_amplicons.apply(lambda row: find_closest_match(str(row["comp_rev"]),reference_seq), axis=1)
+    failed_amplicons["alt_right_primer_loc"] = failed_amplicons.apply(lambda row: find_closest_match(str(row["comp_rev"]),reference_seq,), axis=1)
     merged_df = pd.merge(
         merged_df,
         failed_amplicons[["amplicon_number","alt_left_primer_loc","alt_right_primer_loc"]],
         on=["amplicon_number"],
         how="outer"
     )
-    merged_df["amplicon"] = merged_df.apply(lambda row: make_amplicon(row["left_primer_loc"],
-                                                                          row["right_primer_loc"],
-                                                                          row["alt_left_primer_loc"],
-                                                                          row["alt_right_primer_loc"],
-                                                                          row["primer_seq_y"],
-                                                                          reference_seq), axis=1)
+    merged_df['left_primer_loc'] = merged_df.apply(
+        lambda row: row['alt_left_primer_loc'] if row['left_primer_loc'] == -1 else row['left_primer_loc'],
+        axis=1)
+    merged_df['right_primer_loc'] = merged_df.apply(
+        lambda row: row['alt_right_primer_loc'] if row['right_primer_loc'] == -1 else row['right_primer_loc'],
+        axis=1)
 
+    merged_df["amplicon_length"] = merged_df["right_primer_loc"] - merged_df["left_primer_loc"] + len(merged_df["primer_seq_y"])
+    merged_df["amplicon_sequence"] = merged_df.apply(lambda row: make_amplicon(row["left_primer_loc"],
+                                                                          row["right_primer_loc"],
+                                                                          row["primer_seq_y"],reference_seq), axis=1)
     for row in merged_df.itertuples():
         if not os.path.exists(args.output):
             os.makedirs(args.output)
         with open(f"{args.output}/amplicon_{row.amplicon_number}" + ".fasta", "w") as f:
             f.write(f">{reference.id}_amplicon_{row.amplicon_number}" + "\n")
-            f.write(row.amplicon + "\n\n")
+            f.write(row.amplicon_sequence + "\n\n")
 
 
 if __name__ == "__main__":
