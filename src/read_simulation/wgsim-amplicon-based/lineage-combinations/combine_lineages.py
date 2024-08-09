43e0d50378a2414c911463f398f018e829c3a394
Maryam <mary.elf97@gmail.com>
2024-08-09 14:00:24 -0700

    new changes to run quicker for different sample combinations

--- src/read_simulation/wgsim-amplicon-based/lineage-combinations/combine_lineages.py
+++ src/read_simulation/wgsim-amplicon-based/lineage-combinations/combine_lineages.py
@@ -1,44 +1,35 @@
 import itertools
 import pandas as pd
+import subprocess
 
-ISOLATES = [i for i in open("/home/maryam/mnt/tb_seqs/seq_simulation/amplicon_read_simulations/amplicon_alignment_wgsim/primer_v2/mixed_samples_diff_cov/isolates.txt").read().split('\n') if len(i) >0]
-ABUNDANCES = [i for i in open("/home/maryam/mnt/tb_seqs/seq_simulation/amplicon_read_simulations/amplicon_alignment_wgsim/primer_v2/mixed_samples_diff_cov/abundances.txt").read().split('\n') if len(i) >0]
+import itertools
+import pandas as pd
+import subprocess
+import random
 
+
+ISOLATES = [i for i in open("/home/mahmadi/measles_seqs/assemblies/unique_isolates.txt").read().split('\n') if len(i) >0]
+
+# Generate 4 random proportion combinations
 isolate_combinations = list(itertools.combinations(ISOLATES, 2))
-abundance_combinations = [("25000","75000"),("250000","750000")]
-
+primer_bed_file = "/home/mahmadi/measles_seqs/primers/primer_v3_400.bed"
 for isolate_combination in isolate_combinations:
-    for abundance_combination in abundance_combinations:
-        file1_path = "/home/maryam/mnt/tb_seqs/seq_simulation/amplicon_read_simulations/amplicon_alignment_wgsim/primer_v2/" + isolate_combination[0] + "/" + abundance_combination[0] + "/reads_1.fastq"
-        file2_path = "/home/maryam/mnt/tb_seqs/seq_simulation/amplicon_read_simulations/amplicon_alignment_wgsim/primer_v2/" + isolate_combination[1] + "/" + abundance_combination[1] + "/reads_1.fastq"
-        output_path = "/home/maryam/mnt/tb_seqs/seq_simulation/amplicon_read_simulations/amplicon_alignment_wgsim/primer_v2/mixed_samples_diff_cov/" + isolate_combination[0] + "_" + abundance_combination[0] + "_" + isolate_combination[1] + "_" + abundance_combination[1] + ".fastq"
-        with open(file1_path, 'r') as file1:
-            lines_file1 = file1.readlines()
-        with open(file2_path, 'r') as file2:
-            lines_file2 = file2.readlines()
-        combined_lines = lines_file1 + lines_file2
-        with open(output_path, 'w') as output_file:
-            output_file.writelines(combined_lines)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    proportion1 = round(random.uniform(0, 1), 2)
+    proportion2 = round(1.0 - proportion1, 2)
+    file1_path = "/home/mahmadi/measles_seqs/assemblies/" + isolate_combination[0] + "/" + isolate_combination[0] + ".fna"
+    file2_path = "/home/mahmadi/measles_seqs/assemblies/" + isolate_combination[1] + "/" + isolate_combination[1] + ".fna"
+    output_path = "/home/mahmadi/measles_seqs/seq_simulation/combinations/" + isolate_combination[0] + "_" + str(proportion1) + "_" +  isolate_combination[1] + "_" + str(proportion2)
+    command = [
+            "python", "/home/mahmadi/git_repos/amplicon_sequencing_simulator/workflow/scripts/amplicon_simulator_wrapper.py",
+            "-s",f"{isolate_combination[0]},{isolate_combination[1]}" ,
+            "-sp", f"{file1_path},{file2_path}",
+            "-pr", f"{proportion1},{proportion2}",
+            "-p", primer_bed_file,
+            "-n", "100000",
+            "-o", output_path
+        ]
+        # Execute the command
+    subprocess.run(command, check=True)
 
 
 
